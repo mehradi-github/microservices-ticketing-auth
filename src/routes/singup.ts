598d807ab1c48f83711641efe38571a511249e53
Mehradi <mehradi.info@gmail.com>
2022-10-26 11:04:16 +0300

    Validating request

--- src/routes/singup.ts
+++ src/routes/singup.ts
@@ -1,6 +1,23 @@
-import express from 'express';
+import express,{ Request, Response } from 'express';
+import { validateRequest } from '../middlewares/validate-request';
+
+const { body, validationResult } = require('express-validator');
+
 const router= express.Router();
-router.post('/api/users/singup',(req,res)=>{
+router.post(
+    '/api/users/singup',
+    [
+        body('email')
+          .isEmail()
+          .withMessage('Email must be valid'),
+        body('password')
+          .trim()
+          .isLength({ min: 4, max: 20 })
+          .withMessage('Password must be between 4 and 20 characters')
+      ],
+      validateRequest,
+      (req: Request, res: Response) => {
+    
     res.send('Hi there!');
 });
 
