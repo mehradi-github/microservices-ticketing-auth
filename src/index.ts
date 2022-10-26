94e592e043c798cc2145c280ceec1de1ed7e880b
Mehradi <mehradi.info@gmail.com>
2022-10-26 12:44:14 +0300

    Adding error handler

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import { currentUserRouter } from "./routes/current-user";
 import { signoutRouter } from "./routes/singout";
 import { signinRouter } from "./routes/signin";
 import {signupRouter} from "./routes/singup";
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from "./middlewares/error-handler";
+
 const app = express();
 app.use(json());
 
@@ -11,6 +14,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!");
