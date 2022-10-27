c59c50749729948f05e35ad26a5ca38e6e845d08
Mehradi <mehradi.info@gmail.com>
2022-10-27 11:43:49 +0300

    Adding start func in index

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,9 @@ const start = async () => {
   } catch (err) {
     console.log(err);
   }
-};
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!');
-});
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!');
+  });
+};
+start();
--- src/routes/singup.ts
+++ src/routes/singup.ts
@@ -1,24 +1,22 @@
-import express,{ Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import { validateRequest } from '../middlewares/validate-request';
 
-const { body, validationResult } = require('express-validator');
+import { body, validationResult } from 'express-validator';
 
-const router= express.Router();
+const router = express.Router();
 router.post(
-    '/api/users/singup',
-    [
-        body('email')
-          .isEmail()
-          .withMessage('Email must be valid'),
-        body('password')
-          .trim()
-          .isLength({ min: 4, max: 20 })
-          .withMessage('Password must be between 4 and 20 characters')
-      ],
-      validateRequest,
-      (req: Request, res: Response) => {
-    
+  '/api/users/singup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
     res.send('Hi there!');
-});
+  }
+);
 
-export {router as signupRouter};
+export { router as signupRouter };
