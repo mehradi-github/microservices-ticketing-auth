29ce4904e38b887b2360fc5cbee24c4bd5fe9291
Mehradi <mehradi.info@gmail.com>
2022-10-27 18:12:07 +0300

    Adding current user

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,7 +1,14 @@
-import express from 'express';
-const router= express.Router();
-router.get('/api/users/currentuser',(req,res)=>{
-    res.send('Hi there!');
-});
+import express, { Request, Response } from 'express';
+import { currentUser } from '../middlewares/current-user';
 
-export {router as currentUserRouter};
+const router = express.Router();
+
+router.get(
+  '/api/users/currentuser',
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
+  }
+);
+
+export { router as currentUserRouter };
