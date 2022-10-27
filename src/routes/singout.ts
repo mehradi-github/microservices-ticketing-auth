e8b3daa6254893f7d810da0b8339017664b881ab
Mehradi <mehradi.info@gmail.com>
2022-10-27 18:06:49 +0300

    Adding signout

--- src/routes/singout.ts
+++ src/routes/singout.ts
@@ -1,7 +1,11 @@
-import express from 'express';
-const router= express.Router();
-router.post('/api/users/signout',(req,res)=>{
-    res.send('Hi there!');
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+router.post('/api/users/signout', (req: Request, res: Response) => {
+  req.session = null;
+
+  res.send({});
 });
 
-export {router as signoutRouter};
+export { router as signoutRouter };
